e8ab0a6952a6e12102a4155d0d268864a55cb65b
Alexey Karnachev <alekseykarnachev@gmail.com>
2024-07-19 12:48:03 +0400

    rename textures dir to pbr

--- src/resources.cpp
+++ src/resources.cpp
@@ -23,8 +23,10 @@ void load() {
     DEFAULT_MATERIAL = LoadMaterialDefault();
 
     MATERIALS_PBR = {
-        pbr::MaterialPBR("resources/textures/brick_wall/", {1.0, 1.0}, 0.0),
-        pbr::MaterialPBR("resources/textures/tiled_stone/", {1.0, 1.0}, 0.0),
+        pbr::MaterialPBR("resources/pbr/brick_wall/", {1.0, 1.0}, 0.0),
+        pbr::MaterialPBR("resources/pbr/tiled_stone/", {1.0, 1.0}, 0.0),
+        pbr::MaterialPBR("resources/pbr/modern_shattered_wallpaper/", {1.0, 1.0}, 0.0),
+        pbr::MaterialPBR("resources/pbr/hungarian_point_flooring/", {1.0, 1.0}, 0.0),
     };
 
     PLANE_MESH = gen_mesh_plane(2);
